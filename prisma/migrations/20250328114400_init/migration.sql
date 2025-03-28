4acf6be804c1736b10ccec552b09676c6bd79850
Mahmud <me@imahmud.com>
2025-03-28 17:46:27 +0600

    Create prisma database migration

---
+++ prisma/migrations/20250328114400_init/migration.sql
@@ -0,0 +1,179 @@
+-- CreateEnum
+CREATE TYPE "Profile" AS ENUM ('STUDENT', 'TRAINER', 'COUNSELOR', 'CONTROLLER', 'ADMIN', 'SUPER_ADMIN');
+
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('PENDING', 'ACTIVE', 'INACTIVE', 'SUSPENDED', 'ARCHIVED', 'DELETED', 'BLOCKED', 'DRAFT');
+
+-- CreateEnum
+CREATE TYPE "Gender" AS ENUM ('MALE', 'FEMALE', 'NON_BINARY', 'PREFER_NOT_TO_SAY');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "LastName" TEXT NOT NULL,
+    "address" TEXT,
+    "bio" TEXT,
+    "gender" "Gender",
+    "profile" "Profile" NOT NULL DEFAULT 'STUDENT',
+    "referrerId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserMeta" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "key" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+
+    CONSTRAINT "UserMeta_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProfileMeta" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "userProfile" "Profile" NOT NULL,
+    "key" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+
+    CONSTRAINT "ProfileMeta_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProfileStatus" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "userProfile" "Profile" NOT NULL,
+    "status" "Status" NOT NULL DEFAULT 'INACTIVE',
+
+    CONSTRAINT "ProfileStatus_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Salary" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "userProfile" "Profile" NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "Salary_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Commission" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "userProfile" "Profile" NOT NULL,
+    "commission" DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+
+    CONSTRAINT "Commission_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Balance" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "userProfile" "Profile" NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Balance_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Transaction" (
+    "id" TEXT NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    "reason" TEXT,
+    "details" JSONB,
+    "status" TEXT,
+    "payerId" TEXT NOT NULL,
+    "payeeId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Node" (
+    "id" TEXT NOT NULL,
+    "nodeId" TEXT NOT NULL,
+    "nodeProfile" "Profile" NOT NULL,
+    "mapperId" TEXT NOT NULL,
+    "mapperProfile" "Profile" NOT NULL,
+    "mappedId" TEXT NOT NULL,
+    "mappedProfile" "Profile" NOT NULL,
+
+    CONSTRAINT "Node_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Notification" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "userProfile" "Profile" NOT NULL,
+    "title" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "details" JSONB,
+    "isRead" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "readAt" TIMESTAMP(3),
+
+    CONSTRAINT "Notification_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_id_profile_key" ON "User"("id", "profile");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_profile_key" ON "User"("email", "profile");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phoneNumber_profile_key" ON "User"("phoneNumber", "profile");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_referrerId_fkey" FOREIGN KEY ("referrerId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserMeta" ADD CONSTRAINT "UserMeta_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProfileMeta" ADD CONSTRAINT "ProfileMeta_userId_userProfile_fkey" FOREIGN KEY ("userId", "userProfile") REFERENCES "User"("id", "profile") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProfileStatus" ADD CONSTRAINT "ProfileStatus_userId_userProfile_fkey" FOREIGN KEY ("userId", "userProfile") REFERENCES "User"("id", "profile") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Salary" ADD CONSTRAINT "Salary_userId_userProfile_fkey" FOREIGN KEY ("userId", "userProfile") REFERENCES "User"("id", "profile") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Commission" ADD CONSTRAINT "Commission_userId_userProfile_fkey" FOREIGN KEY ("userId", "userProfile") REFERENCES "User"("id", "profile") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Balance" ADD CONSTRAINT "Balance_userId_userProfile_fkey" FOREIGN KEY ("userId", "userProfile") REFERENCES "User"("id", "profile") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_payerId_fkey" FOREIGN KEY ("payerId") REFERENCES "Balance"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_payeeId_fkey" FOREIGN KEY ("payeeId") REFERENCES "Balance"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Node" ADD CONSTRAINT "Node_nodeId_nodeProfile_fkey" FOREIGN KEY ("nodeId", "nodeProfile") REFERENCES "User"("id", "profile") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Node" ADD CONSTRAINT "Node_mappedId_mapperProfile_fkey" FOREIGN KEY ("mappedId", "mapperProfile") REFERENCES "User"("id", "profile") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Node" ADD CONSTRAINT "Node_mappedId_mappedProfile_fkey" FOREIGN KEY ("mappedId", "mappedProfile") REFERENCES "User"("id", "profile") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Notification" ADD CONSTRAINT "Notification_userId_userProfile_fkey" FOREIGN KEY ("userId", "userProfile") REFERENCES "User"("id", "profile") ON DELETE RESTRICT ON UPDATE CASCADE;

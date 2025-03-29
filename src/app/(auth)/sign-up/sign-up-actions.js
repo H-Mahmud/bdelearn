9804a125bebc24d57ab1ca89b5fde6f9a4a80dbc
Mahmud <me@imahmud.com>
2025-03-29 08:05:54 +0600

    Create sign-up action

--- src/app/(auth)/sign-up/sign-up-actions.js
+++ src/app/(auth)/sign-up/sign-up-actions.js
@@ -1,12 +1,29 @@
 'use server';
 
-import { SignUpSchema } from "src/schema/userSchema";
+import { redirect } from 'next/navigation';
+
+import { paths } from 'src/routes/paths';
+
+import { getUserByReferCode } from 'src/model/user';
+import { SignUpSchema } from 'src/schema/userSchema';
 
 export default async function userSignUp(formData) {
-    const {success} = SignUpSchema.safeParse(formData)
-    if(!success) {
-    throw new Error('validation error')
-    } else {
-        console.log('succss')
-    }
+  const { success, data, error } = SignUpSchema.safeParse(formData);
+  if (!success) {
+    return error.formErrors;
+  }
+
+  const { referralCode } = data;
+  let referrer;
+  try {
+    referrer = await getUserByReferCode(referralCode);
+  } catch (e) {
+    console.log(e);
+  }
+
+  if (!referrer) {
+    return { error: 'referrer user not found' };
+  }
+
+  return redirect(paths.dashboard);
 }

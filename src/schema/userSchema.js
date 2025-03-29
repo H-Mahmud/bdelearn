69f3d1b7e60195bbab9b504be4e443bddd366465
Mahmud <me@imahmud.com>
2025-03-29 08:14:41 +0600

    Add phoneNumber & ReferralCode fields

--- src/schema/userSchema.js
+++ src/schema/userSchema.js
@@ -7,8 +7,22 @@ export const SignUpSchema = zod.object({
     .string()
     .min(1, { message: 'Email is required!' })
     .email({ message: 'Email must be a valid email address!' }),
+  phoneNumber: zod
+    .string()
+    .min(1, { message: 'Whatsapp Number is required!' })
+    .min(10, { message: 'Whatsapp number must be at least 10 digits long' })
+    .max(15, { message: 'Whatsapp number must be no more than 15 digits long' })
+    .regex(/^\+?[0-9]+$/, {
+      message: "Whatsapp number must contain only numbers and an optional '+' at the start",
+    }),
   password: zod
     .string()
     .min(1, { message: 'Password is required!' })
     .min(6, { message: 'Password must be at least 6 characters!' }),
+  referralCode: zod
+    .string()
+    .min(1, { message: 'Referral Code is required!' })
+    .regex(/^\d{8}$/, {
+      message: 'Referral code must be exactly 8 digits and contain only numbers',
+    }),
 });

93581217a0c057f208bd47ad4ab5618838745994
Mahmud <me@imahmud.com>
2025-03-29 09:47:37 +0600

    Create new user action

--- src/app/(auth)/sign-up/sign-up-actions.js
+++ src/app/(auth)/sign-up/sign-up-actions.js
@@ -4,25 +4,49 @@ import { redirect } from 'next/navigation';
 
 import { paths } from 'src/routes/paths';
 
-import { getUserByReferCode } from 'src/model/user';
 import { SignUpSchema } from 'src/schema/userSchema';
+import {
+  createStudent,
+  getStudentByReferCode,
+  isStudentRegisteredByEmail,
+  isStudentRegisteredByPhoneNumber,
+} from 'src/model/user';
 
 export default async function userSignUp(formData) {
-  const { success, data, error } = SignUpSchema.safeParse(formData);
+  const { success, data } = SignUpSchema.safeParse(formData);
   if (!success) {
-    return error.formErrors;
+    return { field: null, error: 'Invalid form data' };
   }
 
-  const { referralCode } = data;
-  let referrer;
+  const { firstName, lastName, email, phoneNumber, referralCode } = data;
+
   try {
-    referrer = await getUserByReferCode(referralCode);
+    if (await isStudentRegisteredByEmail(email)) {
+      return {
+        field: 'email',
+        error: 'An account with this email already exists.',
+      };
+    }
+
+    if (await isStudentRegisteredByPhoneNumber(phoneNumber)) {
+      return {
+        field: 'phoneNumber',
+        error: 'An account with this phone number already exists.',
+      };
+    }
+
+    const referrer = await getStudentByReferCode(referralCode);
+    if (!referrer) {
+      return {
+        field: 'referralCode',
+        error: 'Invalid referral Code, No student found by the referral code.',
+      };
+    }
+
+    await createStudent(firstName, lastName, email, phoneNumber, referrer);
   } catch (e) {
     console.log(e);
-  }
-
-  if (!referrer) {
-    return { error: 'referrer user not found' };
+    return { field: null, error: 'Unknown server error.' };
   }
 
   return redirect(paths.dashboard);

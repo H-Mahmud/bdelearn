2e6a6fbc3d45fcbe67e2c74ec31295bbae24ae63
Mahmud <me@imahmud.com>
2025-03-29 23:00:00 +0600

    Complete sign-in action

--- src/app/(auth)/sign-in/sign-in-action.js
+++ src/app/(auth)/sign-in/sign-in-action.js
@@ -1,5 +1,39 @@
 'use server';
 
+import { saltAndHashPassword } from 'src/utils/hash';
+
+import { signIn } from 'src/auth';
+import { SignInSchema } from 'src/schema/userSchema';
+import { getUserByCredential, isUserRegisteredByEmail } from 'src/model/user';
+
 export default async function userSignIn(formData) {
-  console.log(formData);
+  const { data, success } = SignInSchema.safeParse(formData);
+
+  if (!success) {
+    return { field: 'root', error: 'Invalid form data' };
+  }
+
+  const { email, profile, password } = data;
+  try {
+    if (!(await isUserRegisteredByEmail(email, profile))) {
+      return { field: 'email', error: 'No account registered with this email' };
+    }
+
+    const pwHash = saltAndHashPassword(password);
+    const user = await getUserByCredential(email, profile, pwHash);
+    if (!user) {
+      return { field: 'password', error: 'Incorrect password' };
+    }
+    await signIn('credentials', {
+      email,
+      profile,
+      password: pwHash,
+      redirect: false,
+      callbackUrl: '/',
+    });
+    return { success: true, redirectUrl: '/dashboard' };
+  } catch (e) {
+    console.log(e);
+    return { field: 'root', error: 'Sign In failed, Unknown server error.' };
+  }
 }

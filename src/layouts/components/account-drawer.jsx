0fac8d998f1c35129d866aaea191c9dbad45ebbe
Mahmud <me@imahmud.com>
2025-03-30 16:54:29 +0600

    Clean up dashboard content

--- src/layouts/components/account-drawer.jsx
+++ src/layouts/components/account-drawer.jsx
@@ -26,7 +26,6 @@ import { AnimateAvatar } from 'src/components/animate';
 
 import { useMockedUser } from 'src/auth/hooks';
 
-import { UpgradeBlock } from './nav-upgrade';
 import { AccountButton } from './account-button';
 import { SignOutButton } from './sign-out-button';
 
@@ -195,10 +194,6 @@ export function AccountDrawer({ data = [], sx, ...other }) {
               );
             })}
           </Stack>
-
-          <Box sx={{ px: 2.5, py: 3 }}>
-            <UpgradeBlock />
-          </Box>
         </Scrollbar>
 
         <Box sx={{ p: 2.5 }}>

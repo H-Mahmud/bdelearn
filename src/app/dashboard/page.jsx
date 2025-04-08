fed514c4f98a6dc8e1ddcde25a82f079bd375f9c
Mahmud <me@imahmud.com>
2025-04-08 22:35:35 +0600

    Add dashboard page

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -6,6 +6,6 @@ import { BlankView } from 'src/sections/blank/view';
 
 export const metadata = { title: `Dashboard - ${CONFIG.site.name}` };
 
-export default function Page() {
-  return <BlankView title="Page one" />;
+export default function DashboardPage() {
+  return <BlankView title="Dashboard Page" />;
 }

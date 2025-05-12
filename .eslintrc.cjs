664a885be9e4aed4348bd12bc7c390a1ee3bb201
Mahmud <me@imahmud.com>
2025-05-12 22:38:05 +0600

    Update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   plugins: ['perfectionist', 'unused-imports', 'prettier'],
-  extends: ['airbnb', 'airbnb/hooks', 'prettier'],
+  extends: ['airbnb', 'airbnb/hooks', 'prettier',  'plugin:@next/next/recommended',],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
